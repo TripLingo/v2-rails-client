9baaecd62215838c100f0dc06b201f3c9e651e0e
John <john@triplingo.com>
2012-10-12 02:40:46 -0400

    products json

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #user = User.create! :email => "user@example.com", :password => "doorkeeper", :password_confirmation => "doorkeeper"
-require 'rest-client'
-require 'json'
-
-client_id = 'my_client_id...'
-client_secret = 'my_client_secret...'
-
-response = RestClient.post 'http://localhost:3000/oauth/token', {
-  grant_type: 'client_credentials',
-  client_id: client_id,
-  client_secret: client_secret
-}
-token = JSON.parse(response)["access_token"]
-
-# this line works great:
-RestClient.get 'http://localhost:3000/api/v1/flights.json', { 'Authorization' => "Bearer #{token}" }
-# this line always fails (401 Unauthorized):
-RestClient.post 'http://localhost:3000/api/v1/flights.json', { 'Authorization' => "Bearer #{token}" }
+# require 'rest-client'
+# require 'json'
+# 
+# client_id = 'my_client_id...'
+# client_secret = 'my_client_secret...'
+# 
+# response = RestClient.post 'http://localhost:3000/oauth/token', {
+  # grant_type: 'client_credentials',
+  # client_id: client_id,
+  # client_secret: client_secret
+# }
+# token = JSON.parse(response)["access_token"]
+# 
+# # this line works great:
+# RestClient.get 'http://localhost:3000/api/v1/flights.json', { 'Authorization' => "Bearer #{token}" }
+# # this line always fails (401 Unauthorized):
+# RestClient.post 'http://localhost:3000/api/v1/flights.json', { 'Authorization' => "Bearer #{token}" }
